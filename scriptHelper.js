4c27b4d686aa763c123928e3567ad72df3c0ddd3
Colin Thurston <c.thursty59@gmail.com>
2021-08-26 14:36:54 -0400

    confused by failures, testing if statements

--- scriptHelper.js
+++ scriptHelper.js
@@ -50,18 +50,21 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    if(fuelLevel >= 10000 || cargoLevel <= 10000){
     launchStatus.innerHTML = "Shuttle is Ready for Launch"
     launchStatus.style.color  = 'rgb(65, 159, 106)';
+    document.getElementById("cargoStatus").innerHTML = 'Cargo mass low enough for launch';
+    document.getElementById("fuelStatus").innerHTML = 'Fuel level high enough for launch';
     }
-
+    if(cargoLevel > 10000){
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+        launchStatus.style.color  = 'rgb(199, 37, 78)';
+        document.getElementById("cargoStatus").innerHTML = 'Cargo mass too heavy for launch';
+        document.getElementById("fuelStatus").innerHTML = 'Fuel level high enough for launch';
+    } 
    if(fuelLevel < 10000) {
         launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
         launchStatus.style.color  = 'rgb(199, 37, 78)';
-        document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`;
+        document.getElementById("fuelStatus").innerHTML = 'Fuel level too low for launch';
    } 
-   if(cargoLevel > 10000){
-       launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-       launchStatus.style.color  = 'rgb(199, 37, 78)';
-       document.getElementById("cargoStatus").innerHTML = 'Cargo mass too heavy for launch';
-   } 
+
 }
 
 async function myFetch() {
