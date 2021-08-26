278e4af6c547befa28533a57e0c03093d468a267
Colin Thurston <c.thursty59@gmail.com>
2021-08-26 02:37:36 -0400

    troublshooting 3

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ window.addEventListener("load", function() {
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
-
    let form = document.querySelector("form");
+   let list = document.getElementById("faultyItems");
+
    form.addEventListener("submit", function(event){
         event.preventDefault();
-        let pilotInput = this.document.querySelector("input[name=pilotName]");
-        let copilotInput = this.document.querySelector("input[name=copilotName]");
-        let fuelInput = this.document.querySelector("input[name=fuelLevel]");
-        let cargoInput = this.document.querySelector("input[name=cargoMass]");
+
+        let pilotInput = document.querySelector("input[name=pilotName]");
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let fuelInput = document.querySelector("input[name=fuelLevel]");
+        let cargoInput = document.querySelector("input[name=cargoMass]");
 
         const pilotValue = pilotInput.value;
         const copilotValue = copilotInput.value;
         const fuelValue = fuelInput.value;
         const cargoValue = cargoInput.value;
-        let list = document.getElementById("faultyItems");
+
 
         formSubmission(document, list, pilotValue, copilotValue, fuelValue, cargoValue);
    });
@@ -30,7 +32,7 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       const planet = pickPlanet(listedPlanets);
+       let planet = pickPlanet(listedPlanets);
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.imageUrl);
    });
    
